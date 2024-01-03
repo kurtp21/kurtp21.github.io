e89b6dc5586725d1f2c9d4cdc0545bb46fd13fd4
kurtpagal <zja641@usask.ca>
2024-01-03 17:40:55 -0600

    Initial commit

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import './styles/tailwind.css';
+
+function App() {
+  return (
+    <div className="flex items-center justify-center text-3xl text-blue-500 font-bold">
+      Hello, World!
+    </div>
+  );
+}
+
+export default App;
