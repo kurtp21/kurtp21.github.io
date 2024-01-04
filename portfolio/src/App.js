7299ae043b56a44f1a14444b5bc2b0a8f8ac7f29
kurtpagal <zja641@usask.ca>
2024-01-03 20:50:31 -0600

    Tested to see if changes are visible.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,8 +3,8 @@ import './styles/tailwind.css';
 
 function App() {
   return (
-    <div className="flex items-center justify-center text-3xl text-blue-500 font-bold">
-      Hello, World!
+    <div className='flex bg-gray-800'>
+      
     </div>
   );
 }
