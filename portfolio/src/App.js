1f0268f1ddb180db789c3907a63d65b5cc731323
Kurt P <kurtpagal@gmail.com>
2024-01-09 23:07:29 -0600

    Fully made the navbar dynamic, making it collapsable.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,3 +1,5 @@
+import "./styles/tailwind.css"
+
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import Home from './components/Home.js';
@@ -7,11 +9,13 @@ import './styles/tailwind.css';
 
 function App() {
   return (
-    <div className='flex flex-col justify-between min-w-screen min-h-screen'>
+    <div className='flex flex-col justify-between overflow-y-auto min-w-screen min-h-screen'>
       <Header />
-      <Routes>
-        <Route path="/" element={ <Home /> }/>
-      </Routes>
+      <div className="">
+        <Routes>
+          <Route path="/" element={ <Home /> }/>
+        </Routes>
+      </div>
       <Footer />
     </div>
   );
--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -1,39 +1,96 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { MdMenu } from "react-icons/md";
 import Logo from "./favicon.png"
 
 const Header = () => {
+    const [ menuVisible, setMenuVisible ] = useState(false);
+
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+    
+    const updateWindowSize = () => {
+        setWindowSize({
+            width: window.innerWidth,
+            height: window.innerHeight,
+        });
+    };
+
+    useEffect(() => {
+        // Add event listener when the component mounts
+        window.addEventListener('resize', updateWindowSize);
+        console.log(windowSize.width, windowSize.height);
+
+        // Remove event listener when the component unmounts
+        return () => {
+            window.removeEventListener('resize', updateWindowSize);
+        };
+
+    }, []); 
+
+    const toggleMenu = () => {
+        setMenuVisible(!menuVisible);
+    }
+
+    const handleMouseEnter = () => {
+        setMenuVisible(true);
+    }
+
+    const handleMouseLeave = () => {
+        setMenuVisible(false);
+    }
+
     return (
-        <nav className="flex items-center justify-between bg-slate-300 px-8 py-6 sm:px-10 sm:py-8 sticky top-0">
-            <div className='flex flex-1 gap-2 items-center justify-left'>
-                <img 
-                    className="flex-0 w-8 h-8"
-                    src={Logo} alt="Logo"    
-                />
-                
-                <h1 className="text-2xl font-semibold tracking-wide">
-                    Kurt Pagal
-                </h1>
-            </div>
-
-            <div className="flex-0 items-center">
-                <div className="text-4xl sm:hidden">
-                    <button className="hover:text-gray-500">
-                        <MdMenu />
-                    </button>
+        <nav>
+            <div className="flex items-start justify-between px-8 py-6 sm:px-10 sm:py-8 sticky top-0">
+                <div className='flex flex-1 gap-2 items-center justify-left'>
+                    <img 
+                        className="flex-0 w-8 h-8"
+                        src={Logo} alt="Logo"    
+                    />
+                    
+                    <h1 className="text-3xl font-semibold font-sans tracking-wide">
+                        Kurt Pagal
+                    </h1>
                 </div>
 
-                <div className="flex gap-4 max-sm:hidden">
-                    <a className="text-sm sm:text-xl">
-                        About Me
-                    </a>
-                    <a className="text-sm sm:text-xl">
-                        Experience
-                    </a>
-                    <a className="text-sm sm:text-xl">
-                        Projects
-                    </a>
-                </div>
+                { windowSize.width < 1025 ? (
+                    <div className="flex flex-col gap-4 items-end"
+                        onMouseEnter={ handleMouseEnter }
+                        onMouseLeave={ handleMouseLeave }
+                    >
+                        <button className="text-4xl hover:text-grey-500" onClick={ toggleMenu }>
+                            <MdMenu />
+                        </button>
+                        
+                        { menuVisible && (
+                            <div className="flex flex-col gap-4 items-end">
+                                <a className="text-2xl font-sans">
+                                    About Me
+                                </a>
+                                <a className="text-2xl font-sans">
+                                    Experience
+                                </a>
+                                <a className="text-2xl font-sans">
+                                    Projects
+                                </a>
+                            </div>
+                        )}
+                    </div>
+                ) : (
+                    <div className="flex gap-4">
+                        <a className="text-2xl font-sans">
+                            About Me
+                        </a>
+                        <a className="text-2xl font-sans">
+                            Experience
+                        </a>
+                        <a className="text-2xl font-sans">
+                            Projects
+                        </a>
+                    </div>
+                ) }
             </div>
         </nav>
     );
